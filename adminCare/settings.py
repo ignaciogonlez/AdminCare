4ad1f127858dd565d8521d4d59c08c4b6fa57548
ignaciogonlez <ignacio.gonlez@opendeusto.es>
2025-04-07 12:58:23 +0200

    preparacion para despliegue

--- adminCare/settings.py
+++ adminCare/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9+ffz2kw2@zvb&hfsn_x22%%bq_35cp9z7f!8x5=%p2_u_o5(j'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'clave_por_defecto_segura')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
