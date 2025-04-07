ea4bb764dd338cfc6b135a99c47c1a0cc1d41f4c
ignaciogonlez <ignacio.gonlez@opendeusto.es>
2025-04-07 13:33:59 +0200

    despliegue 4

--- adminCare/settings.py
+++ adminCare/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']  # para los ficheros estáticos en la carpeta "static" del proyecto
 STATIC_ROOT = BASE_DIR / 'staticfiles'    # en caso de producción, para recolectar ficheros
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Configuración de archivos multimedia
 MEDIA_URL = '/media/'
