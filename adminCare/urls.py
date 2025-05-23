fac5a962154a5921ea58fc784958abef365bcea8
ignaciogonlez <ignacio.gonlez@opendeusto.es>
2025-05-23 17:16:37 +0200

    mail urls

--- adminCare/urls.py
+++ adminCare/urls.py
@@ -1,29 +1,54 @@
 """
 URL configuration for adminCare project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+El `urlpatterns` list routes URLs a vistas. Para más info: https://docs.djangoproject.com/en/5.1/topics/http/urls/
 """
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-path('admin/', admin.site.urls), 
-path('', include('appAdminCare.urls')),
-# Auth de Django (login/logout/password-reset…)
-path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('appAdminCare.urls')),
+
+    # Personalización de restablecimiento de contraseña:
+    path(
+        'accounts/password_reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html',
+            email_template_name='registration/password_reset_email.html',
+            subject_template_name='registration/password_reset_subject.txt',
+            domain_override='admincare.onrender.com',  # Forzamos dominio
+            use_https=True,                            # Protocolo HTTPS
+        ),
+        name='password_reset'
+    ),
+    path(
+        'accounts/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='registration/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'accounts/reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'accounts/reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
+
+    # Rutas por defecto de autenticación (login, logout, password_change, etc.)
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
